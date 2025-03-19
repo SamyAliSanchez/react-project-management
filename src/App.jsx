959be282df84f28a4c6c31ce0507b51195ccceb2
Samy Ali Sánchez <102321253+SamyAliSanchez@users.noreply.github.com>
2025-03-19 11:44:42 +0100

    feat: collecting user input with refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [selectProjectId, setSelectProjectId] = useState();
+  const [projects, setProjects] = useState([]);
+
+  const handleStartAddProject = () => {
+    setSelectProjectId(null);
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjects((prevState) => [
+      ...prevState,
+      { ...projectData, id: Math.random() },
+    ]);
+    setSelectProjectId(undefined);
+  };
+
+  console.log(projects);
+
+  const renderContent = () => {
+    if (selectProjectId === null)
+      return <NewProject onAdd={handleAddProject} />;
+    else if (selectProjectId === undefined)
+      return <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  };
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {renderContent()}
     </main>
   );
 }
